5052d802914fd5809e432d0a53b4d00447ce87eb
ferniprogrammer <fsanchor@myuax.com>
2022-12-09 11:09:14 +0100

    ej2 acabado

--- ej2.py
+++ ej2.py
@@ -1,1 +1,8 @@
 import sympy
+#Sympy es una extensión de Python que sirve para la resolución de ecuaciones más complejas
+a = int(input ("Ingresa el valor de a: "))
+b = int(input ("Ingresa el valor de b: "))
+n = int(input ("Ingresa el valor de n: "))
+x = int(input ("Ingresa el valor de x: "))
+op = (a * x * b)^n
+print(op)
